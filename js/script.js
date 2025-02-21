0e249993043368fd77ae95c9b60a15c7cdd3845c
Josh Canterbury <joshcanterburyofficial@gmail.com>
2025-02-21 09:31:33 -0600

    Recreating site, finished home page and almost the events

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function thanks() {
+    alert("Thank you for submitting the contact form! We'll be in touch shortly.")
+}
+
+// Used with onclick on the submit button in the contact form...this will display a message when they submit the form/click the button //
+
